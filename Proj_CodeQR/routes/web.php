5471321d1ccb4f69a99f8904e7be0060c50ef3f8
Eric Rudnick <eric.r@escolar.ifrn.edu.br>
2023-09-16 19:35:34 -0300

    criação de views, rotas, controllers...

--- Proj_CodeQR/routes/web.php
+++ Proj_CodeQR/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdmController;
+use App\Http\Controllers\PublicacaoController;
+use App\Http\Controllers\UtensilioController;
+use App\Http\Controllers\VideoController;
+use App\Models\Publicacao;
+use App\Models\Video;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+    //Rota para a página de login de ADM
+Route::get('/adm/login', [AdmController::class, 'index']);
+
+    //Rota simples para a tela onde o ADM pode fazer os cadastros
+Route::post('/cadastros',function(){
+    return view('adm.options');
+});
+
+    //Rotas para os Utensilios
+Route::get('/cadastro/utensilios', [UtensilioController::class, 'create']);
+Route::post('/cadastros', [UtensilioController::class, 'show']);
+
+    //Rotas para as Publicações
+Route::get('/cadastro/publicacao', [PublicacaoController::class, 'create']);  
+Route::post('/cadastros', [PublicacaoController::class, 'show']); 
+
+   //Rotas para os videos
+Route::get('/cadastro/Video', [VideoController::class, 'create']);  
+Route::post('/cadastros', [VideoController::class, 'show']);
+
+    
